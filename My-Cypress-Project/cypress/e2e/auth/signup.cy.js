b2eb8108c3e9fa9cebc730ac5eb268b4bf6b45cf
sjcswank <sjcswank@gmail.com>
2025-09-12 16:22:02 -0500

    parameterized sign up page tests.

--- My-Cypress-Project/cypress/e2e/auth/signup.cy.js
+++ My-Cypress-Project/cypress/e2e/auth/signup.cy.js
@@ -1,4 +1,7 @@
 describe('Sign Up Page', () => {
+  
+  const SIGN_UP_URL = 'http://127.0.0.1:5000/sign-up'
+
   const EMAIL = 'test001@email.com'
   const INVALID_EMAIL = 'a@c'
   const PASSWORD = 'Test001'
@@ -8,39 +11,46 @@ describe('Sign Up Page', () => {
 
   beforeEach('Navigate to Sign Up page and gather inputs', () => {
 
-    cy.visit('http://127.0.0.1:5000/sign-up')
+    cy.visit(SIGN_UP_URL)
     cy.get('#email').as('emailInput')
     cy.get('#password1').as('passwordInput')
     cy.get('#password2').as('confirmPasswordInput')
     cy.contains('Submit').as('submitButton')
   })
   
-  it('QACW-04: Should display "Passwords do no match." error alert.', function () {
-    cy.wrap(this.emailInput).type(EMAIL)
-    cy.wrap(this.passwordInput).type(PASSWORD)
-    cy.wrap(this.confirmPasswordInput).type(NOT_MATCHED_PASSWORD)
+
+   const USERS = [
+    {test: 'QACW-04', email: EMAIL, pass1: PASSWORD, pass2: NOT_MATCHED_PASSWORD, error: 'Passwords do not match.'},
+    {test: 'QACW-05', email: EMAIL, pass1: INVALID_PASSWORD, pass2: INVALID_PASSWORD, error: 'Password must be at least 7 characters long.'},
+    {test: 'QACW-06', email: INVALID_EMAIL, pass1: PASSWORD, pass2: PASSWORD, error: 'Email must be more than 3 characters.'}
+  ]
+
+  it.each(USERS)((user) => `${user.test}: Should display "${user.error}" error alert.`, function (user) {
+    cy.wrap(this.emailInput).type(user.email)
+    cy.wrap(this.passwordInput).type(user.pass1)
+    cy.wrap(this.confirmPasswordInput).type(user.pass2)
     cy.wrap(this.submitButton).click()
 
-    cy.get('body > div.alert.alert-danger.alert-dismissible.fade.show').should('contain', 'Passwords do not match.')
+    cy.get('body > div.alert.alert-danger.alert-dismissible.fade.show').should('contain', user.error)
   })
 
-  it('QACW-05: Should display "Password must be at least 7 characters long." error alert.', function () {
+
+    it('QACW-07: Should display "Email already in use." error alert.', function () {
     cy.wrap(this.emailInput).type(EMAIL)
-    cy.wrap(this.passwordInput).type(INVALID_PASSWORD)
-    cy.wrap(this.confirmPasswordInput).type(INVALID_PASSWORD)
-    cy.wrap(this.submitButton).click()
-
-    cy.get('body > div.alert.alert-danger.alert-dismissible.fade.show').should('contain', 'Password must be at least 7 characters long.')
-  })
-
-  it('QACW-06: Should display "Email must be more than 3 characters long." error alert.', function () {
-    cy.wrap(this.emailInput).type(INVALID_EMAIL)
     cy.wrap(this.passwordInput).type(PASSWORD)
     cy.wrap(this.confirmPasswordInput).type(PASSWORD)
     cy.wrap(this.submitButton).click()
+    cy.get('#logout').click()
 
-    cy.get('body > div.alert.alert-danger.alert-dismissible.fade.show').should('contain', 'Email must be more than 3 characters.')
+    cy.visit(SIGN_UP_URL)
+    cy.get('#email').type(EMAIL)
+    cy.get('#password1').type(PASSWORD)
+    cy.get('#password2').type(PASSWORD)
+    cy.contains('Submit').click
+
+    cy.get('body > div.alert.alert-danger.alert-dismissible.fade.show').should('contain', 'Email already in use.')
   })
+
 
   it('QACW-01: Should navigate to Contats page', function () {
     cy.wrap(this.emailInput).type(EMAIL)
@@ -48,23 +58,7 @@ describe('Sign Up Page', () => {
     cy.wrap(this.confirmPasswordInput).type(PASSWORD)
     cy.wrap(this.submitButton).click()
 
-    cy.url().should('eq', 'http://127.0.0.1:5000/')
-  })
-
-  it('QACW-07: Should display "Email already in use." error alert.', function () {
-    cy.wrap(this.emailInput).type(EMAIL)
-    cy.wrap(this.passwordInput).type(PASSWORD)
-    cy.wrap(this.confirmPasswordInput).type(PASSWORD)
-    cy.wrap(this.submitButton).click()
-    cy.get('#LogOut').click()
-
-    cy.visit('http://127.0.0.1:5000/sign-up')
-    cy.get('#email').type(EMAIL)
-    cy.get('#password1').type(PASSWORD)
-    cy.get('#password2').type(PASSWORD)
-    cy.contains('Submit').click
-
-    cy.get('body > div.alert.alert-danger.alert-dismissible.fade.show').should('contain', 'Email already in use.')
+    cy.get('title').should('contain', 'Contacts')
   })
 
 })
